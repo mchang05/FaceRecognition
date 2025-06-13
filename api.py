e7e2480682dfcff24825a3c2f5066248c0bce17c
mchang05 <mc.chang05@gmail.com>
2025-06-13 18:31:37 +0800

    add get embed

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ import numpy as np
 import cv2
 import tempfile
 from db import MariaDBConnection
-from app import app as face_app, find_similar_in_db, find_profile_in_db
+from app import app as face_app, find_similar_in_db, find_profile_in_db, get_embbeding
 from dotenv import load_dotenv
 import os
 import uuid
@@ -52,6 +52,31 @@ async def search_face(file: UploadFile = File(...), threshold: float = 0.65):
             for row in matches
         ]
         return JSONResponse(content={"matches": results})
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error during face search: {str(e)}")
+    finally:
+        # Delete the temporary file after use
+        if os.path.exists(tmp_path):
+            os.remove(tmp_path)
+            
+@app.post("/get_embed/")
+async def get_embed(file: UploadFile = File(...), threshold: float = 0.65):
+    # Save uploaded file to a temporary location in the current project folder
+    tmp_filename = f"tmp_{uuid.uuid4().hex}.jpg"
+    tmp_path = os.path.join(os.getcwd(), tmp_filename)
+    try:
+        contents = await file.read()
+        with open(tmp_path, "wb") as tmp:
+            tmp.write(contents)
+        print(f"Temporary file saved at: {tmp_path}")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Failed to process uploaded file: {str(e)}")
+
+    # Use find_similar_in_db to search for similar faces
+    try:
+        embedding = get_embbeding(tmp_path)
+        # Format results for JSON response
+        return JSONResponse(content={"embed": embedding})
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error during face search: {str(e)}")
     finally:
--- app.py
+++ app.py
@@ -201,6 +201,17 @@ def find_profile_in_db(img_path, db, threshold=0.6):
         matches.append(fake_row)
     return matches, str(largest_face.bbox.tolist())
 
+def get_embbeding(img_path):
+    
+    img = cv2.imread(img_path)
+    faces = app.get(img)
+    if not faces:
+        print("No face detected in query image.")
+        return None
+
+    largest_face = max(faces, key=lambda f: (f.bbox[2] - f.bbox[0]) * (f.bbox[3] - f.bbox[1]))
+    return largest_face.embedding.tolist()  # Return as a list of floats for compatibility with MariaDB VECTOR type
+
 # Example usage:
 if __name__ == "__main__":
     db = MariaDBConnection()
